f74d7057d1cb663b599866a32bd4fe5261998ecf
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2021-03-21 23:21:53 -0500

    sequelize error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require("./config/connection");
+console.log(sequelize);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
+console.log(process.env.DB_NAME, process.env.DB_PASSWORD,process.env.DB_USER)
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
